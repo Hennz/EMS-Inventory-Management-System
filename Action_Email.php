71de6d7e643fea34b2349f85171bf93cc7c3a714
wra216 <wellesleyarreza@ymail.com>
2014-10-20 17:32:35 -0400

    test

--- Action_Email.php
+++ Action_Email.php
@@ -11,6 +11,7 @@ and open the template in the editor.
     </head>
     <body>
         <?php
+        /*
    $to = "wra216@lehigh.edu";
    $subject = "Jon this is spam from wes";
    $message = "Heyhey... So this is my php script. I don't mean to spam you. Actually.... I do. Haha.";
@@ -23,7 +24,27 @@ and open the template in the editor.
    else
    {
       echo "Message could not be sent...";
+   }*/
+   
+   $to = "wra216@lehigh.edu";
+   $subject = "This is subject";
+   $message = "<b>This is HTML message.</b>";
+   $message .= "<h1>This is headline.</h1>";
+   $header = "From:abc@somedomain.com \r\n";
+   $header = "Cc:afgh@somedomain.com \r\n";
+   $header .= "MIME-Version: 1.0\r\n";
+   $header .= "Content-type: text/html\r\n";
+   $retval = mail ($to,$subject,$message,$header);
+   if( $retval == true )
+   {
+      echo "Message sent successfully...";
    }
+   else
+   {
+      echo "Message could not be sent...";
+   }
+   
+   
 ?>
     </body>
 </html>
