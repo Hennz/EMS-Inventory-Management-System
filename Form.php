a4ef7842794686154ece26c2a9769b7eb392d8ae
Jonathan Chang <firejc5@dyn134065.cc.lehigh.edu>
2014-10-23 16:00:35 -0400

    Made Form page responsive by adding meta tag.

--- Form.php
+++ Form.php
@@ -13,6 +13,7 @@ if (isset($_SESSION['items'])) {
     <html>
         <head>
             <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
             <title></title>
             <!-- Bootstrap core CSS -->
             <link href="./bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
