985262eda37c7f08aad15c48f2429a5510c017de
Jonathan Chang <firejc5@dyn132113.cc.lehigh.edu>
2014-10-23 12:42:59 -0400

    Added Check-in button. Extra styling on button group. etc.

--- Form.php
+++ Form.php
@@ -9,54 +9,54 @@ and open the template in the editor.ssss
 session_start();
 
 if (isset($_SESSION['items'])) {
-?>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-         <!-- Bootstrap core CSS -->
-    <link href="./bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
-<!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    ?>
+    <html>
+        <head>
+            <meta charset="UTF-8">
+            <title></title>
+            <!-- Bootstrap core CSS -->
+            <link href="./bootstrap-3.2.0-dist/css/bootstrap.min.css" rel="stylesheet">
+            <!-- Latest compiled and minified CSS -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+            <!-- Optional theme -->
+            <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
-        
-        
-        
-        
-    </head>
-    <body>
-        
-        <div class="header">
-            
-        </div>
-        
-        <div class="sidebar">
-            
-            
-        </div>
-        
-        
-        <table class="table table-bordered">
-            <tr class="active">
-            <th>Item</th>
-            <th>Quantity</th>
-            <th>Check</th>
-            <th>Amount</th>
-            </tr>
-       <?php   
+            <!-- Latest compiled and minified JavaScript -->
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+
+
+
+        </head>
+        <body>
+
+            <div class="header">
+
+            </div>
+
+            <div class="sidebar">
+
+
+            </div>
+
+
+            <table class="table table-bordered">
+                <tr class="active">
+                    <th>Item</th>
+                    <th>Quantity</th>
+                    <th>Check</th>
+                    <th>Amount</th>
+                </tr>
+                <?php
                 if (isset($_SESSION['items'])) {
-                    $items = $_SESSION['items'];               
-                } 
-           
+                    $items = $_SESSION['items'];
+                }
+
 
                 $i = 0;
                 $size = sizeof($items);
-                
+
                 for ($i = 0; $i < $size; $i++) {
                     $item = $items[$i];
                     echo '<tr>';
@@ -65,35 +65,29 @@ if (isset($_SESSION['items'])) {
                     echo "<td>" . "<input type='checkbox' name$item->id'></td>";
                     echo "<td>" . "<input type='text' name$item->id'></td>";
                     echo '</tr>';
-                  
                 }
-                
-                
-                
-                
                 ?>
-        
-     
-     
-     
-        </table>
-        <input type="button" class="btn btn-primary" value="Checkout">
-        </br>
-        Email Inventory Manager:
-        <form method="get" action='index.php' class="form-signin" role="form">
-        <input type="submit" class="btn btn-primary" value="Email Manager" name="Email">
-        </form>
-    </body>
-</html>
-<?php
-}
 
-else{
-    
+
+
+
+            </table>
+            <div class ="container">
+                <input type="button" class="btn btn-primary" value="Checkin">
+                <input type="button" class ="btn btn-success" value ="Checkout">
+            </div>
+            </br>
+            Email Inventory Manager:
+            <form method="get" action='index.php' class="form-signin" role="form">
+                <input type="submit" class="btn btn-primary" value="Email Manager" name="Email">
+            </form>
+        </body>
+    </html>
+    <?php
+} else {
+
     echo "Woops.";
-   
 }
- unset($_SESSION['items']);
- exit;
-
+unset($_SESSION['items']);
+exit;
 ?>
