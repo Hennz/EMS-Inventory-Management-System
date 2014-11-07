39cd318fbae6d05ebde92840ae7154e297b8e5b8
wra216 <wellesleyarreza@ymail.com>
2014-11-07 00:44:52 -0500

    Entered PHPUNIT for Unit Testing.  LoginTest.php is one of our unit tests for logging in.

---
+++ LoginTest.php
@@ -0,0 +1,49 @@
+<?php
+include 'Action.php';
+include 'InventoryDAO.php';
+class Connect extends PHPUnit_Framework_TestCase  {
+    
+  public function testConnectionIsValid()
+  {
+      
+    $dao= new InventoryDAO();
+    
+    
+    // Lets test invalid usernames and passwords.
+    $invalidUser="bigbooty";
+    $invalidPass="Jchang";
+    
+    
+    $this->assertNotNull($invalidUser);
+    $this->assertNotNull($invalidPass);
+    $this->assertTrue($dao->checkPasswd($invalidUser, $invalidPass) == false);
+    
+    $invalidUser="cb";
+    $invalidPass="Jynx";
+    
+    $this->assertNotNull($invalidUser);
+    $this->assertNotNull($invalidPass);
+    $this->assertTrue($dao->checkPasswd($invalidUser, $invalidPass) == false);
+    
+    $invalidUser="Goggles";
+    $invalidPass="DoubleJynx";
+    $this->assertNotNull($invalidUser);
+    $this->assertNotNull($invalidPass);
+    $this->assertTrue($dao->checkPasswd($invalidUser, $invalidPass) == false);
+    
+    
+    // lets test the valid user name and password
+    $username="Bob";
+    $password="password";
+    $this->assertNotNull($username);
+    $this->assertNotNull($password);
+    $this->assertTrue($dao->checkPasswd($username, $password) !== false);
+  }
+    
+    
+    
+}
+
+
+?>
+
