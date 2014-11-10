c026e3fd522577b817ae1247f4f0ca72c36fe675
Jonathan Chang <firejc5@dyn133030.cc.lehigh.edu>
2014-11-10 15:37:21 -0500

    added subtraction

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,14 @@
 function add(display){
+    display = "db" + display;
     document.getElementByName(display).value = parseFloat(document.getElementByName(display).value) + 1;
+}
+
+function sub(display){
+    display = "db" + display;
+    if (parseFloat(document.getElementByName(display).value) === 0){
+        document.getElementByName(display).value = "0";
+    }
+    else {
+    document.getElementByName(display).value = parseFloat(document.getElementByName(display).value) - 1;
+}
 }
