d7d8152f30bacb772891017069d5b24e8d872af4
Jonathan Chang <firejc5@dyn133030.cc.lehigh.edu>
2014-11-10 15:45:09 -0500

    update

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 function add(display){
     alert(display);
-    display = "db" + display;
-    alert(display);
-    document.getElementByName(display).value = parseFloat(document.getElementByName(display).value) + 1;
+    var displayString = "db" + display;
+    alert(displayString);
+    document.getElementByName(displayString).value = parseFloat(document.getElementByName(displayString).value) + 1;
 }
 
 function sub(display){
-    display = "db" + display;
-    if (parseFloat(document.getElementByName(display).value) === 0){
-        document.getElementByName(display).value = "0";
+    var displayString = "db" + display;
+    if (parseFloat(document.getElementByName(displayString).value) === 0){
+        document.getElementByName(displayString).value = "0";
     }
     else {
-    document.getElementByName(display).value = parseFloat(document.getElementByName(display).value) - 1;
+    document.getElementByName(displayString).value = parseFloat(document.getElementByName(displayString).value) - 1;
 }
 }
