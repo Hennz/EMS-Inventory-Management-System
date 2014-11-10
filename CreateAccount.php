3f4b3b30fd42d51cdc202dba384ece369111843f
cmv216 <cmv216@lehigh.edu>
2014-11-10 16:25:01 +0000

    CreateAccount.php test

--- CreateAccount.php
+++ CreateAccount.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
+By: Emile Bahou & Christopher Vickery
 -->
 <html>
     <head>
@@ -13,3 +11,4 @@ and open the template in the editor.
     
     </body>
 </html>
+
