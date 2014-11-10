9f708bc56a6e8577bcdcc39e024f9727b03b7259
Jonathan Chang <firejc5@dyn133030.cc.lehigh.edu>
2014-11-10 16:24:33 -0500

    commented out all alerts.. do note the comment in javascript file works

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,6 @@
 function add(display){
-    document.getElementById('db1').value = parseFloat(document.getElementById('db1').value) + 1;
-      document.getElementById('1').value = parseFloat(document.getElementById('1').value) + 1;
-    alert(display);
+    //document.getElementById('db1').value = parseFloat(document.getElementById('db1').value) + 1;
     var displayString = "db" + display;
-    alert(displayString);
     document.getElementById(displayString).value = parseFloat(document.getElementById(displayString).value) + 1;
 }
 
