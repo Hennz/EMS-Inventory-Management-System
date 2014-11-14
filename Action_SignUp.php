aa1f07016ec5a9fc50c7a2e50246e9b68cfc5a3f
Chris Vickery <cmv216@lehigh.edu>
2014-11-13 19:53:16 -0500

    basic outline of Action_SignUp

--- Action_SignUp.php
+++ Action_SignUp.php
@@ -3,6 +3,27 @@ session_start();
 
 include 'Action.php';
 
+class Action_SignUp implements Action {
+    
+    public function execute($request) {
+        
+        $dao = new InventoryDAO();
+        $i = 0; //DUMMY VARIABLE; DELETE EVENTUALLY
+        
+        if($i === 0){  //DUMMY CONDITION; REAL CONDITION IS THAT WE CHECK FOR FILLED OUT FIELDS WITH A METHOD USING $dao
+            //
+            
+            //After a successful account creation, redirect to login page.
+            header("Location: login.html"); 
+        }        
+        else{
+            header("Location: CreateAccount.html"); 
+        }
+        
+    }
+    
+}
+    
 
 
 ?>
