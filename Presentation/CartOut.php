246a5a359e254c3ff18e189e57f4499966e9e1a1
wra216 <wellesleyarreza@ymail.com>
2014-12-10 13:57:47 -0500

    testing cartout.php

--- Presentation/CartOut.php
+++ Presentation/CartOut.php
@@ -118,7 +118,7 @@ if (isset($_SESSION['selectedItems'])) {
            
         </body>
         
-        <?php
+       <?php
         
         
         $server="";
@@ -130,10 +130,16 @@ if (isset($_SESSION['selectedItems'])) {
        echo "<script src='".$server."/js/scripts.js'></script>";
        }
        else if($_SERVER["DOCUMENT_ROOT"]=="D:\\home\\site\\wwwroot"){
-           $server="\\";
+           $server="";
            echo "<script src='".$server."\\js\\ajax.js'></script>";
        echo "<script src='".$server."\\js\\scripts.js'></script>";
        }
+        
+      
+       
+       
+       
+        
         ?>
         
         
